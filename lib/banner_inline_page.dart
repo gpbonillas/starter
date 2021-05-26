9292e35df087e4dd3144f168668a21c98f77a611
Pato Bonilla <gpbonillas@gmail.com>
2021-05-26 15:37:08 -0500

    Add a banner ad

--- lib/banner_inline_page.dart
+++ lib/banner_inline_page.dart
@@ -17,6 +17,9 @@
 import 'package:admob_inline_ads_in_flutter/destination.dart';
 
 // TODO: Import google_mobile_ads.dart
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+// TODO: Import ad_helper.dart
+import 'package:admob_inline_ads_in_flutter/ad_helper.dart';
 
 import 'package:flutter/material.dart';
 
@@ -33,18 +36,40 @@ class BannerInlinePage extends StatefulWidget {
 
 class _BannerInlinePageState extends State<BannerInlinePage> {
   // TODO: Add _kAdIndex
+  static final _kAdIndex = 4;
 
   // TODO: Add a BannerAd instance
+  late BannerAd _ad;
 
   // TODO: Add _isAdLoaded
+  bool _isAdLoaded = false;
 
   @override
   void initState() {
     super.initState();
 
     // TODO: Create a BannerAd instance
+    _ad = BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      size: AdSize.banner,
+      request: AdRequest(),
+      listener: AdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isAdLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          // Releases an ad resource when it fails to load
+          ad.dispose();
+
+          print('Ad load failed (code=${error.code} message=${error.message})');
+        },
+      ),
+    );
 
     // TODO: Load an ad
+    _ad.load();
   }
 
   @override
@@ -55,27 +80,35 @@ class _BannerInlinePageState extends State<BannerInlinePage> {
       ),
       body: ListView.builder(
         // TODO: Adjust itemCount based on the ad load state
-        itemCount: widget.entries.length,
+        itemCount: widget.entries.length + (_isAdLoaded ? 1 : 0),
         itemBuilder: (context, index) {
           // TODO: Render a banner ad
+          if (_isAdLoaded && index == _kAdIndex) {
+            return Container(
+              child: AdWidget(ad: _ad),
+              width: _ad.size.width.toDouble(),
+              height: 72.0,
+              alignment: Alignment.center,
+            );
+          } else {
+            // TODO: Get adjusted item index from _getDestinationItemIndex()
+            final item = widget.entries[_getDestinationItemIndex(index)];
 
-          // TODO: Get adjusted item index from _getDestinationItemIndex()
-          final item = widget.entries[index];
-
-          return ListTile(
-            leading: Image.asset(
-              item.asset,
-              width: 48,
-              height: 48,
-              package: 'flutter_gallery_assets',
-              fit: BoxFit.cover,
-            ),
-            title: Text(item.name),
-            subtitle: Text(item.duration),
-            onTap: () {
-              print('Clicked ${item.name}');
-            },
-          );
+            return ListTile(
+              leading: Image.asset(
+                item.asset,
+                width: 48,
+                height: 48,
+                package: 'flutter_gallery_assets',
+                fit: BoxFit.cover,
+              ),
+              title: Text(item.name),
+              subtitle: Text(item.duration),
+              onTap: () {
+                print('Clicked ${item.name}');
+              },
+            );
+          }
         },
       ),
     );
@@ -84,10 +117,15 @@ class _BannerInlinePageState extends State<BannerInlinePage> {
   @override
   void dispose() {
     // TODO: Dispose a BannerAd object
-
+    _ad.dispose();
     super.dispose();
   }
 
 // TODO: Add _getDestinationItemIndex()
-
+  int _getDestinationItemIndex(int rawIndex) {
+    if (rawIndex >= _kAdIndex && _isAdLoaded) {
+      return rawIndex - 1;
+    }
+    return rawIndex;
+  }
 }
